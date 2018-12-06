76335a6fe7b8a406083d7847fb83cb822a2a1312
EstebanEB <estebanlolequisde@gmail.com>
2018-12-06 12:30:50 -0600

    ComandCenter V1
    Armado basico del comand Denter y su ScriptBase

---
+++ Sentai_-RTS-/Assets/Scripts/comandCenter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class comandCenter : MonoBehaviour {
+
+    public Camera gameCamera;
+    public GameObject HUD;
+    public GameObject SelectionRay;
+
+
+	void Start ()
+    {
+        gameCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+	
+
+	void Update ()
+    {
+        showInterface();
+
+    }
+
+    public void showInterface()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);  //raycast en la posicion del clik 
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))  // se guarda la colision
+            {
+                if (SelectionRay = GameObject.FindGameObjectWithTag("ComandC"))
+                     HUD.SetActive(true);
+                
+            }
+
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            HUD.SetActive(false);
+        }
+
+    }
+
+
+    public void ShowBtns()
+    { }
+
+
+}
+
