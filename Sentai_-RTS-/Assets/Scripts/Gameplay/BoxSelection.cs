d51e39d058976ae590abfb0bef01b5a204bc0800
Fernando Soria <39631495+ZeroBlair@users.noreply.github.com>
2018-12-09 01:19:18 -0600

    Creacion del escenario y unidades de recoleccion.

---
+++ Sentai_-RTS-/Assets/Scripts/Gameplay/BoxSelection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSelection : MonoBehaviour
+{
+
+    [SerializeField]
+    private RectTransform boxSeleccion;
+
+    Vector3 mouseinitialPosition;
+    Vector3 mouseFinalPosition;
+
+    Camera thisCamera;
+
+    void Start()
+    {
+        boxSeleccion.gameObject.SetActive(false);
+        thisCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit rayhit;
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayhit, Mathf.Infinity))
+            {
+                mouseinitialPosition = rayhit.point;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            boxSeleccion.gameObject.SetActive(false);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (!boxSeleccion.gameObject.activeInHierarchy)
+            {
+                boxSeleccion.gameObject.SetActive(true);
+            }
+
+            mouseFinalPosition = Input.mousePosition;
+
+            Vector3 boxStart = thisCamera.WorldToScreenPoint(mouseinitialPosition);
+            boxStart.z = 0;
+
+            Vector3 center = (boxStart + mouseFinalPosition) / 2;
+
+            boxSeleccion.position = center;
+            float axisX = Mathf.Abs(boxStart.x - mouseFinalPosition.x);
+            float axisY = Mathf.Abs(boxStart.y - mouseFinalPosition.y);
+
+            boxSeleccion.sizeDelta = new Vector2(axisX, axisY);
+        }
+    }
+}
+
---
+++ Sentai_-RTS-/Assets/Scripts/Gameplay/UnitSelected.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelected : MonoBehaviour
+{
+    [SerializeField]
+    protected Material defaulMaterial, selectedMaterial;
+
+    private MeshRenderer theRend;
+
+    public bool isSelected = false;
+
+    GameObject theCamera;
+
+    void Start()
+    {
+        theRend = GetComponent<MeshRenderer>();
+
+        theCamera = GameObject.Find("Main Camera");
+        theCamera.GetComponent<SelectionSystem>().unitsToSelect.Add(this.gameObject);
+    }
+
+    public void UpdateStatus()
+    {
+        if (!isSelected)
+        {
+            theRend.material = defaulMaterial;
+        }
+        else
+        {
+            theRend.material = selectedMaterial;
+        }
+    }
+}
