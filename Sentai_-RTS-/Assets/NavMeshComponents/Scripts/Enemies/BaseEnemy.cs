32c5ba1fdf3ef190ddbe9fcde5d50fe50aa7c548
EstebanEB <estebanlolequisde@gmail.com>
2018-12-11 11:28:04 -0600

    Scripts IA (Enemigos)
    Se subio los Scripts de los enemigos

---
+++ Sentai_-RTS-/Assets/NavMeshComponents/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class BaseEnemy : MonoBehaviour {
+
+    public int hP;
+    public int speedMovement;
+
+    public Rigidbody rbd;
+    public GameObject target;
+
+    Vector3 initialPosition;
+
+    public Transform[] pathPoints;  //array of positions
+    public int CurrentPath = 0;  //the index of the Path
+    public float ReachDistance = 5.0f; //waypoints colisionZone
+
+
+    protected virtual void Start()
+    {
+        rbd = GetComponent<Rigidbody>();
+        initialPosition = transform.position;
+    }
+
+    
+    void Update () {
+		
+	}
+
+   public void walk()
+    {
+        Vector3 dir = pathPoints[CurrentPath].position - transform.position;  // the direction
+        Vector3 dirNorm = dir.normalized; // normalizing direction
+
+        transform.Translate(dirNorm * (speedMovement * Time.fixedDeltaTime)); //Moves the transform in the direction of the translation
+
+        if (dir.magnitude <= ReachDistance)      // magnitude returns the length of the vector dir, and compare with the reach distance
+        {
+            CurrentPath++;
+         
+
+            if (CurrentPath >= pathPoints.Length)    // start again to follow the wayPoints
+            {
+                CurrentPath = 0;
+            }
+        }
+
+
+    }
+
+    void onDrawGizmos()
+    {
+        if (pathPoints.Length == 0)
+            return;
+
+        foreach (Transform pathPoint in pathPoints) // for every PathPoint draw a sphere
+        {
+            if (pathPoint)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(pathPoint.position, ReachDistance);
+            }
+        }
+    }
+
+}
---
+++ Sentai_-RTS-/Assets/NavMeshComponents/Scripts/Enemies/MeleEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleEnemy : BaseEnemy {
+
+    bool Detection;
+    public float enemyRadarSize = 5;
+
+	protected override void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        selector();
+	}
+
+    public void selector()
+    {
+        if (Detection == false)
+        {
+            walk();
+        }
+        else
+        {
+           follow();
+        }
+    }
+
+    public void follow()
+    {
+
+
+
+    }
+
+
+   
+}
