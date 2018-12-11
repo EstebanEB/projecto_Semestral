539293c57b983a1ec8d8fa07496b592cc7c854a4
EstebanEB <estebanlolequisde@gmail.com>
2018-12-11 00:48:38 -0600

    Nav Mesh package

---
+++ Sentai_-RTS-/Assets/NavMeshComponents/Scripts/Controljugador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Controljugador : MonoBehaviour {
+
+    public NavMeshAgent agenteplayer;
+
+    public Camera gameCamera;
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);  //raycast en la posicion del clik del maus
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))  // se guarda la colision we
+            {
+                agenteplayer.SetDestination(hit.point);
+            }
+        }
+		
+	}
+}
